ae828ee7a0fdadd5bacb4ff2c3d0b51508405d48
Alizayd <85483108+Alizayd@users.noreply.github.com>
2021-08-01 11:54:37 +0300

    هذا الquiz

---
+++ js/js.js
@@ -0,0 +1,19 @@
+function view(){
+    var yesNo = document.getElementById('date').style.display ;
+    if( yesNo == "block")
+    {
+        document.getElementById('date').style.display = "none";
+    }else {
+        document.getElementById('date').style.display = "block"
+    }
+}
+function hi(){
+    var yesNo = document.getElementById('hesilf').style.display ;
+    if( yesNo == "block")
+    {
+        document.getElementById('hesilf').style.display = "none";
+    }else {
+        document.getElementById('hesilf').style.display = "block"
+    }
+}
+
